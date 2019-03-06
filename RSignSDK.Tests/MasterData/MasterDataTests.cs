644adf4567e3a71a1040cc5f25034fa7ac226f59
Adam Stirtan <adamstirtan@gmail.com>
2019-03-06 11:17:57 -0500

    Added tests for MasterData

---
+++ RSignSDK.Tests/MasterData/MasterDataTests.cs
@@ -0,0 +1,46 @@
+﻿using System.Linq;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace RSignSDK.Tests.MasterData
+{
+    [TestClass]
+    public class MasterDataTests : BaseApiTest
+    {
+        [TestMethod]
+        public void GetDateFormatsTest()
+        {
+            var sut = new RSignAPI(GetCredentials());
+
+            try
+            {
+                var list = sut.GetDateFormats();
+
+                Assert.IsNotNull(list);
+                Assert.AreNotEqual(list.Count(), 0);
+            }
+            catch (AuthenticationException)
+            {
+                Assert.Fail();
+            }
+        }
+
+        [TestMethod]
+        public void GetFontsTest()
+        {
+            var sut = new RSignAPI(GetCredentials());
+
+            try
+            {
+                var list = sut.GetFonts();
+
+                Assert.IsNotNull(list);
+                Assert.AreNotEqual(list.Count(), 0);
+            }
+            catch (AuthenticationException)
+            {
+                Assert.Fail();
+            }
+        }
+    }
+}
