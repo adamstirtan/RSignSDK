e418392bdeffe6a9389586ad5ab6fa7268db2920
Adam Stirtan <adamstirtan@gmail.com>
2019-03-06 10:44:04 -0500

    Added unit test project

---
+++ RSignSDK.Tests/BaseApiTest.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace RSignSDK.Tests
+{
+    [TestClass]
+    public class BaseApiTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
