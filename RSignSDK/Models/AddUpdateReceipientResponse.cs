16c2b4efbfa8699dcf2efa7623da994dcb8cd73d
Lorcan Quinn <36154051+LorcanQ@users.noreply.github.com>
2019-04-18 12:37:23 +0100

    Adding AddUpdate request/response classes

--- RSignSDK/Models/AddUpdateReceipientResponse.cs
+++ RSignSDK/Models/AddUpdateReceipientResponse.cs
@@ -1,5 +1,12 @@
 ﻿namespace RSignSDK.Models
 {
     public class AddUpdateReceipientResponse
-    { }
+    {
+        public string EnvelopeID { get; set; }
+        public string RecipientType { get; set; }
+        public string RecipientName { get; set; }
+        public string Email { get; set; }
+        public int Order { get; set; }
+        public string RecipientID { get; set; }
+    }
 }
