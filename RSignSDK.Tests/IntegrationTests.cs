79cb438ebe7a1ea5e9c5c61f7a88c2e1bbb747d4
Adam Stirtan <adamstirtan@icloud.com>
2019-04-18 10:04:48 -0400

    Test scaffolding

---
+++ RSignSDK.Tests/IntegrationTests.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RSignSDK.Models;
+using System.Linq;
+
+namespace RSignSDK.Tests
+{
+    [TestClass]
+    public class IntegrationTests : BaseApiTest
+    {
+        [TestMethod]
+        public void SendingEnvelopeTest()
+        {
+            //using (var sut = new RSignAPI(GetCredentials()))
+            //{
+            //    var initializeEnvelopeResponse = sut.InitializeEnvelope(new InitializeEnvelopeRequest
+            //    {
+            //    });
+
+            //    var templates = sut.GetTemplates();
+            //    var template = templates.Where(x => x.Name == "integration_test")
+
+            //    Assert.AreEqual(initializeEnvelopeResponse.StatusCode, "200");
+
+            //    sut.UseTemplate(new UseTemplateRequest
+            //    {
+            //        TemplateID
+            //    })
+            //}
+        }
+    }
+}
