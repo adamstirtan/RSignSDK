be7ad9e0e69997b6339ebef45ea16078b75e08d1
Lorcan Quinn <36154051+LorcanQ@users.noreply.github.com>
2019-05-20 16:57:37 +0100

    Adding UploadLocalDocument Request/response classes

---
+++ RSignSDK/Models/UploadLocalDocumentRequest.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace RSignSDK.Models
+{
+    public class UploadLocalDocumentRequest
+    {
+        public string FileName { get; set; }
+        public string EnvelopeId { get; set; }
+        public string DocumentBase64Data { get; set; }
+        public string EnvelopeStage { get; set; }
+    }
+}
